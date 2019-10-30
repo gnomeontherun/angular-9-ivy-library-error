1443b1139d0a2d814d0acd1c2dbd8d1c1cc05547
Jeremy Wilken <gnomation@gnomeontherun.com>
2019-10-29 20:47:20 -0500

    use library in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClrAngularModule } from '@clr/angular';
+// import { ClrAngularModule } from '../../projects/clr-angular/src/index';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClrAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
